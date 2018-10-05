165b515442fa61add6dabe57e92b12d1260dc5aa
devprojef <devprojef@gmail.com>
2018-10-05 18:20:54 +0300

    category controller init

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Laravel\Lumen\Routing\Controller;
+
+class CategoryController extends Controller
+{
+    public function add(Request $request): JsonResponse
+    {
+
+    }
+
+    public function retrieve(Request $request): JsonResponse
+    {
+
+    }
+}
