64581e4eaf7dea839104a24b112b1ed40400ad59
devprojef <devprojef@gmail.com>
2018-10-05 18:20:03 +0300

    category routes init

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\File;
 
 $router->get('/', function () use ($router)
 {
-    return '';
+    return File::get(app()->basePath() . '/public/index.html');
 });
 
+$router->group(['prefix' => 'api'], function () use ($router)
+{
+    $router->post('categories', ['uses' => 'CategoryController@add']);
+
+    $router->get('categories', ['uses' => 'CategoryController@retrieve']);
+});
